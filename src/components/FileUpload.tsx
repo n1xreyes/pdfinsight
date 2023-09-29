b1fc3cfd7c9c5f50e59d44442bedf628d32fb288
Angelo <n1xreyes@gmail.com>
2023-09-29 14:13:47 -0400

    added s3 integration with file upload component

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,11 +1,31 @@
 'use client';
+import { uploadToS3 } from '@/lib/s3';
 import { Inbox } from 'lucide-react';
 import React from 'react'
 import { useDropzone } from 'react-dropzone';
 
 const FileUpload = () => {
 
-  const { getRootProps, getInputProps} = useDropzone();
+  const { getRootProps, getInputProps} = useDropzone({
+    accept: {'application/pdf': ['.pdf']},
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
+      console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      // if bigger than 10mb, don't upload
+      if (file.size > 10 * 1024 * 1024) {
+        alert('Your file size is more than 10mb. Please upload a smaller file size');
+        return;
+      }
+
+      try {
+      const data = await uploadToS3(file);
+      console.log('data', data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
 
   return (
     <section className='p-2 bg-white rounded-xl'>
